8ef39f58eaf9338cf61aba3387f9e3ffe7570de3
Ryu_woong <jin02014@naver.com>
2023-04-11 23:27:24 +0900

    카운팅까지는 분할 완료
    근데 저 모든 정보를 오브젝트 형식으로 가져와서하면 더 편할 듯...?

--- src/App.js
+++ src/App.js
@@ -1,67 +1,87 @@
 /* eslint-disable */
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  
-  let [title,setTitle] = useState(["남자 코트 추천","강남역 맛집","요즘 유행하는 것들"]);
-  let [likeCount,setLikeCount] = useState(0);
-  const [modal, setModal] = useState(false);
+    let [title, setTitle] = useState(["남자 코트 추천", "강남역 맛집", "반드시 해낸다"]);
+    let [likeCount, setLikeCount] = useState([0, 0, 0]);
+    const [date, setDate] = useState(["2018-01-14", "2022-06-04", "2022-06-05"]);
+    const [modal, setModal] = useState(false);
 
-  return (
-    <div className="App">
-      <div className="black-nav">
-        <h4>ReactBlog</h4>
-      </div>
-      <button onClick={()=>{
-        setTitle([...title].sort())}}>정렬</button>
-
-      <div className="list">
-        <h4>{title[0]} <span onClick={()=>{setLikeCount(likeCount+1)}}>👍</span> {likeCount} </h4>
-        <p>2023-04-10 발행 <button onClick={()=>{
-          let copyTitle = [...title];
-          copyTitle[0] = "여자 코트 추천";
-          setTitle(copyTitle)}}>제목 바꾸기</button></p> 
-        
-      </div>
-      <div className="list">
-        <h4>{title[1]}</h4>
-        <p>2023-04-10 발행</p>
-      </div>
-      <div className="list">
-        <h4 onClick={()=> {setModal(!modal)}}>{title[2]}</h4>
-        <p>2023-04-10 발행</p>
-      </div>
-
-      {
-        //조건시 ? 참일때 실행할 코드 : 거짓일 때 실행할 코드
-        modal === true ? <Modal/> : null
-      }
-      
-
-    </div>
-  );
-
-  function Mine(){
     return (
-      <div className=''>
-        <p>여기는 컴포넌트 내부</p>
-      </div>
-    )
-  }
+        <div className="App">
+            <div className="black-nav">
+                <h4>ReactBlog</h4>
+            </div>
+            <button
+                onClick={() => {
+                    setTitle([...title].sort());
+                }}
+            >
+                정렬
+            </button>
 
+            {title.map(function (element, i) {
+                return (
+                    <div className="list" key={i}>
+                        <h4
+                            onClick={() => {
+                                setModal(!modal);
+                            }}
+                        >
+                            {element}
+                            <span
+                                onClick={() => {
+                                    let copyCount = [...likeCount];
+                                    copyCount[i] = copyCount[i] + 1;
+                                    setLikeCount(copyCount);
+                                }}
+                            >
+                                👍
+                            </span>
+                            {likeCount[i]}
+                        </h4>
+                        <p>
+                            {date[i]} 발행
+                            <button
+                                onClick={() => {
+                                    let copyTitle = [...title];
+                                    copyTitle[0] = "여자 코트 추천";
+                                    setTitle(copyTitle);
+                                }}
+                            >
+                                제목 바꾸기
+                            </button>
+                        </p>
+                    </div>
+                );
+            })}
 
-  function Modal(){
-    return(
-      <div className='modal'>
-          <h4>제목</h4>
-          <p>날짜</p>
-          <p>상세내용</p>
-      </div>
-    )
-  }
+            {
+                //조건시 ? 참일때 실행할 코드 : 거짓일 때 실행할 코드
+                modal === true ? <Modal /> : null
+            }
+        </div>
+    );
+}
 
+function Mine() {
+    return (
+        <div className="">
+            <p>여기는 컴포넌트 내부</p>
+        </div>
+    );
+}
+
+function Modal() {
+    return (
+        <div className="modal">
+            <h4>제목</h4>
+            <p>날짜</p>
+            <p>상세내용</p>
+        </div>
+    );
 }
 
 export default App;
