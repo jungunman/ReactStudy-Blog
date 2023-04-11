781b995cd60864e47f1d617c3d21cc7ad193f61e
Ryu_woong <jin02014@naver.com>
2023-04-11 21:25:24 +0900

    Modal 만들고 삼항연산자 사용해서 Modal 제어하기
    State를 스위치 삼아서 잘 사용해봅시다.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   
   let [title,setTitle] = useState(["남자 코트 추천","강남역 맛집","요즘 유행하는 것들"]);
   let [likeCount,setLikeCount] = useState(0);
+  const [modal, setModal] = useState(false);
 
   return (
     <div className="App">
@@ -29,11 +30,38 @@ function App() {
         <p>2023-04-10 발행</p>
       </div>
       <div className="list">
-        <h4>{title[2]}</h4>
+        <h4 onClick={()=> {setModal(!modal)}}>{title[2]}</h4>
         <p>2023-04-10 발행</p>
       </div>
+
+      {
+        //조건시 ? 참일때 실행할 코드 : 거짓일 때 실행할 코드
+        modal === true ? <Modal/> : null
+      }
+      
+
     </div>
   );
+
+  function Mine(){
+    return (
+      <div className=''>
+        <p>여기는 컴포넌트 내부</p>
+      </div>
+    )
+  }
+
+
+  function Modal(){
+    return(
+      <div className='modal'>
+          <h4>제목</h4>
+          <p>날짜</p>
+          <p>상세내용</p>
+      </div>
+    )
+  }
+
 }
 
 export default App;
